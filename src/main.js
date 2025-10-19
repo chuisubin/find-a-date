87f2c5b22ab422d842b8cc248b2460f31a894a30
chuisubin <chuisubin@gmail.com>
2025-10-20 04:13:37 +0800

    Add v-calendar dependency and integrate it into the main application

--- src/main.js
+++ src/main.js
@@ -3,8 +3,11 @@ import { createPinia } from "pinia";
 import App from "./App.vue";
 import "./style.css";
 import router from "./router";
+import vcalendar from "./plugins/vcalendar";
+import "v-calendar/style.css";
 
 const app = createApp(App);
 app.use(createPinia());
 app.use(router);
+app.use(vcalendar);
 app.mount("#app");

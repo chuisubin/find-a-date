8a98e8ebf4cf6aa999d77ed6a6dd60cc66883eaa
chuisubin <chuisubin@gmail.com>
2025-10-28 15:18:35 +0800

    feat: update dependencies and remove v-calendar integration

--- src/main.js
+++ src/main.js
@@ -19,8 +19,6 @@ import { createPinia } from "pinia";
 import App from "./App.vue";
 import "./style.css";
 import router from "./router";
-import vcalendar from "./plugins/vcalendar";
-import "v-calendar/style.css";
 import "vue3-toastify/dist/index.css";
 import Vue3Toasity, { toast } from "vue3-toastify";
 import FloatingVue from "floating-vue";
@@ -40,7 +38,6 @@ library.add(
 app.component("FontAwesomeIcon", FontAwesomeIcon);
 app.use(createPinia());
 app.use(router);
-app.use(vcalendar);
 app.use(FloatingVue);
 app.use(Vue3Toasity, {
   autoClose: 3000, //自動關閉時間

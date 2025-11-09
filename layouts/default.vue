8b0bc2e73428ed8a832ffaa9f9ef3661d7a1da62
chuisubin <chuisubin@gmail.com>
2025-11-09 11:40:47 +0800

    style: reformat layout structure for improved readability

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,17 +1,25 @@
 <template>
-    <div class="min-h-screen  relative bg-background-light  dark:bg-background-dark text-text-light  dark:text-text-dark">
- <main class=" px-4  lg:px-10  ">
-    <div class="container mx-auto py-8  relative min-h-[calc(100vh-6.25rem)] ">
-  <slot />
-</div>
- 
-</main>
- <footer class="h-[6.25rem] bg-gray-200 dark:bg-gray-700 text-center py-4 mt-auto text-gray-600 dark:text-gray-300">
-      <p>Made with ❤️</p>
-      <p class="">&copy;  2025 Angus Chui | 個人作品</p>
+  <div
+    class="min-h-screen relative bg-background-light dark:bg-background-dark text-text-light dark:text-text-dark"
+  >
+    <main class="px-4 lg:px-10">
+      <div class="container mx-auto py-8 relative min-h-[calc(100vh-6.25rem)]">
+        <slot />
+      </div>
+    </main>
+    <footer
+      class=" bg-gray-200 dark:bg-gray-700 text-center py-4 mt-auto text-gray-600 dark:text-gray-300"
+    >
+      <p>Made with ❤️ - <a
+          href="https://forms.gle/DsLENsXdWQbQoCnN6"
+          target="_blank"
+          class="text-primary-light hover:underline"
+        >
+          分享您的意見
+        </a></p>
+      <p class="">&copy; 2025 Angus Chui | 個人作品</p>
     </footer>
-    </div>
+  </div>
 </template>
 
-<script setup>
-</script>
+<script setup></script>

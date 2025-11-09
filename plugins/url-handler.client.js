4d8c943bda4ceba1edffc9f04f87016f483a61b5
chuisubin <chuisubin@gmail.com>
2025-11-09 10:05:48 +0800

    refactor: remove commented navigation event listener code for clarity

--- plugins/url-handler.client.js
+++ plugins/url-handler.client.js
@@ -1,12 +1,13 @@
-if ("navigation" in window) {
-  window.navigation.addEventListener("navigate", (event) => {
-    const url = new URL(event.destination.url);
-    console.log("Navigated to:", url.href);
-
-    // 在這裡處理特定的路由邏輯
-    if (url.pathname.startsWith("/event")) {
-      // 例如：導航到事件頁面
-      window.location.href = url.href;
-    }
-  });
-}
+export default defineNuxtPlugin(() => {
+  // if ("navigation" in window) {
+  //   window.navigation.addEventListener("navigate", (event) => {
+  //     const url = new URL(event.destination.url);
+  //     console.log("Navigated to:", url.href);
+  //     // 在這裡處理特定的路由邏輯
+  //     if (url.pathname.startsWith("/event")) {
+  //       // 例如：導航到事件頁面
+  //       window.location.href = url.href;
+  //     }
+  //   });
+  // }
+});
